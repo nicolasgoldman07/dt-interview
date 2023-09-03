3f4892fb24859042570a52d4afca106b529ff077
Nicolas Goldman <nicolasgoldman07@gmail.com>
2023-09-03 11:13:06 +0200

    chore(project-config): Add project configuration files
    6 files are updated/added in this commit:
    - app.config.ts with minor changes to be able to run the app locally
    - packages chages
    - api config automatic files
    - vscode configurations for the project

--- app.config.ts
+++ app.config.ts
@@ -1,14 +1,19 @@
 import type { CliOptions } from 'dt-app';
 
 const config: CliOptions = {
-  environmentUrl: 'https://azg39614.live.dynatrace.com/',
+  // live -> apps: https://community.dynatrace.com/t5/Developer-Q-A-Forum/Unable-to-start-dev-server-404-logging-into-environment/m-p/218977
+  environmentUrl: 'https://azg39614.apps.dynatrace.com/',
   app: {
     name: 'Dynatrace Test Interview',
     version: '0.0.0',
     description: 'A starting project with routing, fetching data, and charting',
     id: 'my.dynatrace.test.interview',
-    scopes: [{ name: 'storage:logs:read', comment: 'default template' }, { name: 'storage:buckets:read', comment: 'default template' }]
+    scopes: [
+      { name: 'storage:logs:read', comment: 'default template' },
+      { name: 'storage:buckets:read', comment: 'default template' }
+    ],
   },
+  icon: './src/assets/Dynatrace_Logo.svg',
 };
 
 module.exports = config;
